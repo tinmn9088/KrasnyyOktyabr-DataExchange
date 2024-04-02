48a33b427a944da04e2f31b6d6914d1e192e54b9
tinmn9088 <tinman.8.9@yandex.ru>
2024-04-02 21:42:21 +0300

    [JsonTransform] Add `DivideExpression`

---
+++ KrasnyyOktyabr.JsonTransform/Expressions/Creation/JsonDivideExpressionFactory.cs
@@ -0,0 +1,32 @@
+﻿using KrasnyyOktyabr.JsonTransform.Numerics;
+using Newtonsoft.Json.Linq;
+using NJsonSchema;
+using static KrasnyyOktyabr.JsonTransform.Expressions.Creation.JsonNumberExpressionFactoriesHelper;
+
+namespace KrasnyyOktyabr.JsonTransform.Expressions.Creation;
+
+public sealed class JsonDivideExpressionFactory(IJsonAbstractExpressionFactory factory) : IJsonExpressionFactory<DivideExpression>
+{
+    public static string JsonSchemaPropertyDiv => "$div";
+
+    private static readonly Lazy<JsonSchema> s_jsonSchema = new(() => BuildJsonSchema(expressionName: JsonSchemaPropertyDiv).Result);
+
+    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
+    public DivideExpression Create(JToken input)
+    {
+        GetExpressions(
+            input,
+            expressionName: JsonSchemaPropertyDiv,
+            factory,
+            out IExpression<Task<Number>> leftExpression,
+            out IExpression<Task<Number>> rightExpression);
+
+        return new DivideExpression(leftExpression, rightExpression);
+    }
+
+    public bool Match(JToken value)
+    {
+        return s_jsonSchema.Value.Validate(value).Count == 0;
+    }
+}
---
+++ KrasnyyOktyabr.JsonTransform/Expressions/DivideExpression.cs
@@ -0,0 +1,16 @@
+﻿using KrasnyyOktyabr.JsonTransform.Numerics;
+
+namespace KrasnyyOktyabr.JsonTransform.Expressions;
+
+/// <summary>
+/// Divides 2 numbers.
+/// </summary>
+/// <exception cref="ArgumentNullException"></exception>
+public sealed class DivideExpression(IExpression<Task<Number>> leftExpression, IExpression<Task<Number>> rightExpression) : AbstractBinaryNumberExpression(leftExpression, rightExpression)
+{
+    /// <exception cref="DivideByZeroException"></exception>
+    protected override Number Calculate(Number left, Number right)
+    {
+        return left / right;
+    }
+}
