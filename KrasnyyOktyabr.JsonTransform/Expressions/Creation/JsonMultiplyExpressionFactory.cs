b2c66e54640bdc7118294d7a881eaa3743e080a7
tinmn9088 <tinman.8.9@yandex.ru>
2024-04-03 20:42:43 +0300

    [JsonTransform] Fix `JsonSchemaPropertyMultiply` value

--- KrasnyyOktyabr.JsonTransform/Expressions/Creation/JsonMultiplyExpressionFactory.cs
+++ KrasnyyOktyabr.JsonTransform/Expressions/Creation/JsonMultiplyExpressionFactory.cs
@@ -5,7 +5,7 @@ namespace KrasnyyOktyabr.JsonTransform.Expressions.Creation;
 public sealed class JsonMultiplyExpressionFactory(IJsonAbstractExpressionFactory factory)
     : AbstractJsonNumberExpressionFactory<MultiplyExpression>(JsonSchemaPropertyMultiply, factory)
 {
-    public static string JsonSchemaPropertyMultiply => "$multiply";
+    public static string JsonSchemaPropertyMultiply => "$mul";
 
     protected override MultiplyExpression CreateExpressionInstance(IExpression<Task<Number>> leftExpression, IExpression<Task<Number>> rightExpression)
     {
