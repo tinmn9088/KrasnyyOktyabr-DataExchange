78b9a03e85da17c5fa3d57d70eec3bb62095ec3f
tinmn9088 <tinman.8.9@yandex.ru>
2024-04-03 21:38:57 +0300

    [JsonTransform] Add `RoundExpression` with unit tests

---
+++ KrasnyyOktyabr.JsonTransform.Tests/Expressions/RoundExpressionTests.cs
@@ -0,0 +1,36 @@
+﻿using KrasnyyOktyabr.JsonTransform.Numerics;
+using Moq;
+using static KrasnyyOktyabr.JsonTransform.Tests.TestsHelper;
+
+namespace KrasnyyOktyabr.JsonTransform.Expressions.Tests;
+
+[TestClass]
+public class RoundExpressionTests
+{
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void RoundExpression_WhenValueExpressionNull_ShouldThrowArgumentNullException()
+    {
+        new RoundExpression(null!);
+    }
+
+    [TestMethod]
+    public async Task InterpretAsync_ShouldReturnRounded()
+    {
+        Number valueExpressionResult = new(2.4);
+
+        // Setting up value expression
+        Mock<IExpression<Task<Number>>> valueExpressionMock = new();
+        valueExpressionMock
+            .Setup(e => e.InterpretAsync(It.IsAny<IContext>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult(valueExpressionResult));
+
+        RoundExpression roundExpression = new(valueExpressionMock.Object);
+
+        Number expected = new(2);
+
+        Number actual = await roundExpression.InterpretAsync(GetEmptyExpressionContext(), default);
+
+        Assert.AreEqual(expected, actual);
+    }
+}
---
+++ KrasnyyOktyabr.JsonTransform/Expressions/RoundExpression.cs
@@ -0,0 +1,21 @@
+﻿using KrasnyyOktyabr.JsonTransform.Numerics;
+
+namespace KrasnyyOktyabr.JsonTransform.Expressions;
+
+public sealed class RoundExpression(IExpression<Task<Number>> valueExpression) : AbstractUnaryNumberExpression(valueExpression)
+{
+    protected override Number Calculate(Number value)
+    {
+        if (value.Int != null)
+        {
+            return value;
+        }
+
+        if (value.Double != null)
+        {
+            return new(Math.Round(value.Double.Value));
+        }
+
+        throw new NotImplementedException();
+    }
+}
