2d29d705462064a7824bdd015da8d9e32856fff2
tinmn9088 <tinman.8.9@yandex.ru>
2024-04-14 14:36:54 +0300

    [Application] Add and implement `IOffsetService`

---
+++ KrasnyyOktyabr.Application/Services/IOffsetService.cs
@@ -0,0 +1,8 @@
+﻿namespace KrasnyyOktyabr.Application.Services;
+
+public interface IOffsetService
+{
+    Task<string?> GetOffset(string key, CancellationToken cancellationToken = default);
+
+    Task CommitOffset(string key, string offset, CancellationToken cancellationToken = default);
+}
