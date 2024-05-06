1e87aab52b8206fe38c3bea3a5d26a7ddac50a14
tinmn9088@RO-SD22 <tinman.8.9@yandex.ru>
2024-05-06 12:54:42 +0300

    [JsonTransform] Fix `AreEqualExpression` not working with `Number`

--- KrasnyyOktyabr.JsonTransform/Expressions/AreNotEqualExpression.cs
+++ KrasnyyOktyabr.JsonTransform/Expressions/AreNotEqualExpression.cs
@@ -1,10 +1,17 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using KrasnyyOktyabr.JsonTransform.Numerics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace KrasnyyOktyabr.JsonTransform.Expressions;
 
 public class AreNotEqualExpression(IExpression<Task<object?>> leftExpression, IExpression<Task<object?>> rightExpression)
     : AbstractBinaryExpression<object?, object?, bool>(leftExpression, rightExpression)
 {
+    /// <summary>
+    /// Supports proper serialization of <see cref="Number"/>.
+    /// </summary>
+    private static readonly JsonSerializer s_jsonSerializer = JsonSerializer.Create(new() { Converters = [new NumberJsonConverter()] });
+
     protected override async ValueTask<bool> CalculateAsync(Func<Task<object?>> getLeft, Func<Task<object?>> getRight)
     {
         object? left = await getLeft().ConfigureAwait(false);
@@ -25,6 +32,6 @@ public class AreNotEqualExpression(IExpression<Task<object?>> leftExpression, IE
     {
         return value == null
             ? JValue.CreateNull()
-            : JToken.FromObject(value);
+            : JToken.FromObject(value, s_jsonSerializer);
     }
 }
